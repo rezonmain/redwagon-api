94a48cb8b017b16a173dad575614d2539492c2de
rezonmain <adiazgomez101@gmail.com>
2020-12-28 00:47:50 -0800

    linting

--- src/index.js
+++ src/index.js
@@ -3,36 +3,35 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
-const loyverseApi = require("./loyverse.js")
+const loyverseApi = require('./loyverse.js');
 
 const app = express();
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 min
-	max: 10 // No more than 10 request a minute
+	max: 10, // No more than 10 request a minute
 });
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(cors());
 app.use(limiter);
 app.use(helmet());
 
-app.get("/", async (req, res, next) => {
+app.get('/', async (req, res, next) => {
 	res.json({
 		message: "Hello! This endpoint is redwagon's API 🍔",
-	})
-
+	});
 });
 
-app.get("/number", async (req, res, next) => {
+app.get('/number', async (req, res, next) => {
 	try {
-			const burgers = await loyverseApi.numberOfBurgers();
-			res.json({
-				'number_burgers' : burgers,
-				'timestamp': Date.now()
-			});
-	} catch(e) {
-			next(e);
+		const burgers = await loyverseApi.numberOfBurgers();
+		res.json({
+			number_burgers: burgers,
+			timestamp: Date.now(),
+		});
+	} catch (e) {
+		next(e);
 	}
 });
 
@@ -50,7 +49,7 @@ app.use((error, req, res, next) => {
 		stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
 	});
 });
-  
+
 const port = process.env.PORT || 3000;
 const url = process.env.NODE_ENV === 'production' ? 'https://redwagon-api.herokuapp.com/' : `http://localhost:${port}`;
 app.listen(port, () => {
--- src/loyverse.js
+++ src/loyverse.js
@@ -1,23 +1,26 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 const api_key = process.env.API_KEY;
 const auth_header = { Authorization: `Bearer ${api_key}` };
 
 module.exports.numberOfBurgers = function () {
 	return new Promise((resolve, reject) => {
-		// Everything happens here!! 
+		// Everything happens here!!
 		fetch(`https://api.loyverse.com/v1.0/items?limit=250`, {
-			method: "get",
+			method: 'get',
 			headers: auth_header,
 		})
 			.then(handleBadResponse)
-			.then(res => res.json()) // This returns data as json
-			.then(jsonData => getBurgersId(jsonData)) // This returns id_s
-			.then(id_s => burgerSoldToday(id_s)) // Returns number of burgers
-			.then(number => resolve(number)) // Resolve returns the fufilled promise with the number
-			.catch(error => {console.log(error); reject(error)})
+			.then((res) => res.json()) // This returns data as json
+			.then((jsonData) => getBurgersId(jsonData)) // This returns id_s
+			.then((id_s) => burgerSoldToday(id_s)) // Returns number of burgers
+			.then((number) => resolve(number)) // Resolve returns the fufilled promise with the number
+			.catch((error) => {
+				console.log(error);
+				reject(error);
+			});
 	});
-}
+};
 
 function handleBadResponse(response) {
 	if (!response.ok) {
@@ -29,15 +32,15 @@ function handleBadResponse(response) {
 // Get items with id of the burgers category (Basically get the item id of the burgers)
 function getBurgersId(response) {
 	let id_s = {};
-    let category_id = '9a3d4344-71b4-11ea-8d93-0603130a05b8'
-    let j = 0;
+	let category_id = '9a3d4344-71b4-11ea-8d93-0603130a05b8';
+	let j = 0;
 
-    for (let i = 0; i < response['items'].length; i++){
-        let element = response['items'][i]['category_id'];
-        if (element == category_id) {
-            id_s[j] = response['items'][i]['id'];
-            j++;
-        }
+	for (let i = 0; i < response['items'].length; i++) {
+		let element = response['items'][i]['category_id'];
+		if (element == category_id) {
+			id_s[j] = response['items'][i]['id'];
+			j++;
+		}
 	}
 	return id_s;
 }
@@ -66,20 +69,20 @@ function getOpeningDate() {
 		}
 		date = year + '-' + month + '-' + day;
 	}
-	date = `${year}-${month}-${day}T15:00:00.000Z` // Una hora antes de abrir
+	date = `${year}-${month}-${day}T15:00:00.000Z`; // Una hora antes de abrir
 	return date;
 }
 
-// Extract burgers sold from todays receipts 
+// Extract burgers sold from todays receipts
 function burgerSoldToday(id_s) {
 	return new Promise((resolve, reject) => {
 		date = getOpeningDate();
-		console.log('Date sent to API: '+ date);
+		console.log('Date sent to API: ' + date);
 		fetch(`https://api.loyverse.com/v1.0/receipts?created_at_min=${date}&limit=250`, {
-			method: "get",
+			method: 'get',
 			headers: auth_header,
 		})
-			.then(response => response.json())
+			.then((response) => response.json())
 			.then((response) => {
 				let burgersSold = {};
 				let element = '';
@@ -87,7 +90,7 @@ function burgerSoldToday(id_s) {
 
 				for (let i = 0; i < response['receipts'].length; i++) {
 					for (let j = 0; j < response['receipts'][i]['line_items'].length; j++) {
-						for(let x in id_s) {
+						for (let x in id_s) {
 							element = response['receipts'][i]['line_items'][j]['item_id'];
 							if (id_s[x] == element) {
 								burgersSold[p] = {};
@@ -101,14 +104,14 @@ function burgerSoldToday(id_s) {
 				}
 				return burgersSold;
 			})
-			.then(burgersSold => getNumber(burgersSold))
-			.then(number => resolve(number)); // Resolve returns the promise with the number
-		})
+			.then((burgersSold) => getNumber(burgersSold))
+			.then((number) => resolve(number)); // Resolve returns the promise with the number
+	});
 }
 
 // Count number of burgers
 function getNumber(burgersSold) {
-	let numberOfBurgers = 0
+	let numberOfBurgers = 0;
 	for (let i in burgersSold) {
 		numberOfBurgers = numberOfBurgers + burgersSold[i]['cantidad'];
 	}
